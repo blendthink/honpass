7546e3a388d866b20d3ecd360335730438ec8d81
Tatsuya Okayama <t_okayama@yumemi.co.jp>
2020-02-27 21:22:46 +0900

    テストのグループを修正

--- test_driver/add_account_test.dart
+++ test_driver/add_account_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Counter App', () {
+  group('Honpass App', () {
 
     FlutterDriver driver;
 
